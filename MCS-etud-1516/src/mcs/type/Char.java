84506ebffbc5f7e964ddf2cc150f5f657c56da5c
Damien Hostettler <hostetd@bde.enseeiht.fr>
2016-05-11 14:05:22 +0200

    Char

---
+++ MCS-etud-1516/src/mcs/type/Char.java
@@ -0,0 +1,23 @@
+package mcs.type;
+
+public class Char implements DTYPE {
+
+	@Override
+	public int getTaille() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean compareTo(DTYPE autre) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
