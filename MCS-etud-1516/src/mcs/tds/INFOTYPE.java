4b37e918282e529cafc387666c954aebc2d98f7d
qchen <qi.chen@etu.enseeiht.fr>
2016-05-11 15:51:06 +0200

    INFO FONC,TYPE Pointeur

--- MCS-etud-1516/src/mcs/tds/INFOTYPE.java
+++ MCS-etud-1516/src/mcs/tds/INFOTYPE.java
@@ -4,26 +4,19 @@ import mcs.type.DTYPE;
 
 public class INFOTYPE implements INFO {
 
-	// le nom 
+	// le type
 	protected DTYPE type;
-	protected int taille;
 	
-	public INFOTYPE(DTYPE dt,int t) {
+	public INFOTYPE(DTYPE dt) {
 		type = dt;
-		taille = t;
 	}
 	
 	public DTYPE getType() {
 		return this.type;
 	}
-	
-	public int getTaille() {
-		return this.getTaille();
-	}
-	
 	// affichage
 	public String toString() {
-		return "; INFOTYPE sur : " + "TAILLE =" + taille ;
+		return "; INFOTYPE sur : " + "TYPE =" + type ;
 	}
 
 }
