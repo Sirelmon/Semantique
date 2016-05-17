d9a129a5f050fe95b453ab3ec5a2b052abfeec82
qchen <qi.chen@etu.enseeiht.fr>
2016-05-17 18:15:13 +0200

    AVANCER TDS TYPE

--- MCS-etud-1516/src/mcs/tds/INFOFONC.java
+++ MCS-etud-1516/src/mcs/tds/INFOFONC.java
@@ -18,6 +18,7 @@ public class INFOFONC implements INFO{
 
 	protected DTYPE Rtype;
 	protected LinkedHashMap<String, INFO> paras;
+	protected TDS tds;
 	protected String etiq;
 	
 	public DTYPE getRtype() {
@@ -32,10 +33,15 @@ public class INFOFONC implements INFO{
 		return etiq;
 	}
 
-	public INFOFONC(String nom, DTYPE type, LinkedHashMap<String,INFO> para, Emplacement e){
+	public TDS getTds() {
+		return tds;
+	}
+
+	public INFOFONC(String nom, DTYPE type, LinkedHashMap<String,INFO> para, TDS t){
 		this.Rtype =type;
 		this.paras = para;
-		this.etiq = nom+e.toString();
+		this.etiq = nom;
+		this.tds = t;
 	}
 	public String toString(){
 		return ";FONC: étiquette:"+etiq+" ,type de retour:"+Rtype+" ,paras d'entrée:"+paras+'\n';
