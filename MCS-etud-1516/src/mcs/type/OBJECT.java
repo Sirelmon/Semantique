81c5b1f9cacd7f89060bc0a62bb0e906dc32f666
smaurel <smaurel@diabolo.enseeiht.fr>
2016-06-16 10:56:08 +0200

    avancée C sharp

---
+++ MCS-etud-1516/src/mcs/type/OBJECT.java
@@ -0,0 +1,33 @@
+package mcs.type;
+
+import mcs.tds.TDS;
+
+public class OBJECT extends DTYPE {
+	
+	protected TDS tds;
+	
+	public OBJECT() {
+		super("object",0);
+		this.tds = new TDS();
+	}
+	
+	public OBJECT(OBJECT heritage) {
+		super("object",heritage.getTaille());
+		this.tds = new TDS(heritage.tds);
+	}
+		
+	public TDS getTds() {
+		return tds;
+	}
+	
+	public void majTaille(DTYPE type) {
+		this.taille += type.getTaille(); 
+	}
+	
+	public void setTaille() {
+		int taille = 0;
+		for (INFO i : tds.values()) {
+			
+		}
+	}
+}
